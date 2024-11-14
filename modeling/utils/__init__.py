c634a6e0f8173ee5b9801177440c0bedd24720ff
Devik Gupta <64042688+guptadevik2003@users.noreply.github.com>
2024-11-14 21:57:08 +0530

    Packaging utils

---
+++ modeling/utils/__init__.py
@@ -0,0 +1,4 @@
+# modeling/utils/__init__.py
+from .helpers import load_data
+
+__all__ = ['load_data']
--- modeling/utils/helpers.py
+++ modeling/utils/helpers.py
@@ -1,4 +1,4 @@
-# helpers.py
+# modeling/utils/helpers.py
 import pandas as pd
 
 def load_data(data_path):
