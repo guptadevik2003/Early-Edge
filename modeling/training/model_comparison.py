dfc671bf11a2359c785d11991c3371e65e9c6466
Devik Gupta <64042688+guptadevik2003@users.noreply.github.com>
2024-11-14 15:17:18 +0530

    Update model_comparison.py

--- modeling/training/model_comparison.py
+++ modeling/training/model_comparison.py
@@ -6,5 +6,6 @@ def compare_models(data):
   for index, model in enumerate(data):
     print(f"{models[index]}:")
     print(f"Accuracy: {model['accuracy']*100:.2f}%")
-    # print(f"Classification Report:\n{model['report']}")
+    print(f"MSE     : {model['mse']*100:.2f}%")
+    print(f"Classification Report:\n{model['report']}")
     print(f"Confusion Matrix:\n{model['matrix']}\n\n\n")
